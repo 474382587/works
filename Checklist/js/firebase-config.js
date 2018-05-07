f71ad3f06789ee63ca697a40c7f8d3e69dfe239f
Joseph Jin <jinyuhui61@hotmail.com>
2018-05-07 09:52:48 -0700

    checklist

---
+++ Checklist/js/firebase-config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBboVmMUWpvMJguKsiVx2NexWbueciDDvA",
+    authDomain: "checklist-60ef5.firebaseapp.com",
+    databaseURL: "https://checklist-60ef5.firebaseio.com",
+    projectId: "checklist-60ef5",
+    storageBucket: "checklist-60ef5.appspot.com",
+    messagingSenderId: "893586939073"
+};
+firebase.initializeApp(config);

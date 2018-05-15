865edbabf64b84aac539b64aad93d2ae2195d8e6
Joseph <jinyuhui61@hotmail.com>
2018-05-14 20:40:18 -0700

    home page

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,3 +1,4 @@
+//初始化SWIPER插件
 var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
@@ -14,88 +15,122 @@ var mySwiper = new Swiper('.swiper-container', {
     },
 })
 
+moment.locale('zh-cn');
+//計算相對時間
+function calculateTime(millSec) {
+    var date = new Date(millSec*1000);
+    console.log(moment(date).fromNow());
+    console.log(date);
+    return moment(date).fromNow();
+}
+// 1526308894
+
+// console.log(moment("12212121").fromNow());
 
 requestPosts(0, null);
-
+//更新文章
 function updatePosts(arr) {
     let posts = document.querySelector('ul.posts');
     posts.innerHTML = '';
-    for(let i =0; i<arr.length;i++){
-        console.log(i);
+    for (let i = 0; i < arr.length; i++) {
         let li = document.createElement('li');
-        li.innerHTML = `<div class="left"><p>${arr[i].nick_name} ${arr[i].modify_time}</p>${arr[i].title}</div><div class="right"><span>${arr[i].comment_num}</span><span>${arr[i].like_num}</span></div>`
+        li.innerHTML = `<div class="left"><p>${arr[i].nick_name} <span>${calculateTime(arr[i].modify_time)}</span></p>${arr[i].title}</div><div class="right"><span><img src="./static/img/comment-alt.svg" alt="" height="20px">${arr[i].comment_num}</span><span><img src="./static/img/thumbs-up.svg" alt="" height="20px">${arr[i].like_num}</span></div>`
         posts.appendChild(li);
     }
 }
 var currentType;
 const tabs = document.querySelectorAll('ul.tab>li');
-var previous = document.querySelectorAll('ul.tab>li')[0];
-console.log(tabs);
-tabs.forEach((el,index)=>{
-    el.addEventListener('click',(event)=>{
+var previousNode = document.querySelectorAll('ul.tab>li')[0];
+var currentPageNumber = 0;
+const previous = document.querySelector('button.previous');
+const next = document.querySelector('button.next');
+
+//監聽前後頁腳點擊
+previous.addEventListener('click', e => {
+    let posts = document.querySelector('ul.posts');
+    posts.innerHTML = '';
+    requestPosts(currentType, currentPageNumber - 1);
+});
+next.addEventListener('click', e => {
+    let posts = document.querySelector('ul.posts');
+    posts.innerHTML = '';
+    requestPosts(currentType, currentPageNumber + 1);
+});
+//文章分類
+tabs.forEach((el, index) => {
+    el.addEventListener('click', (event) => {
         //加载中
         let posts = document.querySelector('ul.posts');
         posts.innerHTML = '';
-        previous.classList.remove('active');
+        previousNode.classList.remove('active');
         el.classList.add('active');
-        previous = el;
-        console.log(index);
+        previousNode = el;
         requestPosts(index, null);
     });
 });
 
-function requestPosts(type,pageNum){
+//獲取文章
+function requestPosts(type, pageNum) {
     let loading = document.querySelector('div.loader');
     loading.classList.remove('hide');
+    let paginationContainer = document.querySelector('div.pagination-container');
+
+    paginationContainer.classList.add('hide');
     axios({
         method: 'post',
         url: 'http://www.ftusix.com/static/data/topicList.php',
         params: {
             "type": type,
             "sort": "new",
-            "page": pageNum+1,
+            "page": pageNum + 1,
             "index": true,
         }
     }).then(function (response) {
-        console.log(response.data.data);
         let arr = response.data.data;
         let listCount = response.data.listCount[0] - '';
-        console.log(listCount);
+
         let loading = document.querySelector('div.loader');
         loading.classList.add('hide');
         updatePosts(arr);
-        updatePagination(listCount,pageNum);
+        updatePagination(listCount, pageNum);
+        paginationContainer.classList.remove('hide');
         currentType = type;
     });
 }
+//更新頁腳
+function updatePagination(total, index) {
+    index === null ? index = 0 : '';
+    let count = Math.ceil(total / 10);
+    currentPageNumber = index;
 
-function updatePagination(total,index) {
-    index === null? index = 0:'';
-    let count = Math.ceil(total/10);
-    
-    console.log("count "+count)
+    if (currentPageNumber === 0) {
+        previous.classList.add('hide');
+        next.classList.remove('hide');
+
+    } else if (currentPageNumber === count - 1) {
+        next.classList.add('hide');
+        previous.classList.remove('hide');
+
+    } else {
+        previous.classList.remove('hide');
+        next.classList.remove('hide');
+    }
+
     let pagination = document.querySelector('div.pagination');
     pagination.innerHTML = '';
-    for(let i =1; i< count+1; i++){
+    for (let i = 1; i < count + 1; i++) {
         let span = document.createElement('span');
         span.innerText = i;
         pagination.appendChild(span);
     }
-    console.log(index);
-    // if(index === 0){
-    // console.log(document.querySelector('button.previous'));
-    //     document.querySelector('button.previous').setAttribute('disabled',"disabled")
-    // }
-    //button 还没写好 ^
+
     document.querySelector('div.pagination').querySelectorAll('span')[index].classList.add('active');
     var numbers = document.querySelectorAll("div.pagination span");
-    numbers.forEach((el,index)=>{
-        el.addEventListener('click',e=>{
+    numbers.forEach((el, index) => {
+        el.addEventListener('click', e => {
             let posts = document.querySelector('ul.posts');
             posts.innerHTML = '';
-            console.log(currentType)
-            console.log(index)
-            requestPosts(currentType,index);
+            requestPosts(currentType, index);
         })
     })
 }

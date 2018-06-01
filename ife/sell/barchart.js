3107798d529c0156dbdbe8a82b65a45fed8fc05a
Joseph Jin <jinyuhui61@hotmail.com>
2018-06-01 14:24:19 -0700

    barchart

--- ife/sell/barchart.js
+++ ife/sell/barchart.js
@@ -5,18 +5,7 @@ class Barchart {
         this.data = data.splice(2);
     }
 
-
-
-    createChart() {
-        // let test = [120, 110, 140, 160, 180, 185, 190, 210, 230, 245, 255, 270];
-        
-        let width = window.innerWidth / 2.5,
-            height = window.innerWidth / 4;
-        let svg = this.createSVG("svg", {
-            width: width,
-            height: height,
-            viewBox: "0,0,400,250"
-        });
+    createStatic(){
         let yAxis = this.createSVG("line", {
             x1: 30,
             x2: 30,
@@ -34,12 +23,30 @@ class Barchart {
         let title = this.createSVG("text", {
             x: 180,
             y: 245,
-            "font-size":12
+            "font-size": 12
         });
         title.innerHTML = this.title;
-        svg.appendChild(yAxis);
-        svg.appendChild(xAxis);
-        svg.appendChild(title);
+        return {
+            x:xAxis,
+            y:yAxis,
+            title:title
+        }
+    }
+
+
+    createChart() {        
+        let width = window.innerWidth / 2.5,
+            height = window.innerWidth / 4;
+        let svg = this.createSVG("svg", {
+            width: width,
+            height: height,
+            viewBox: "0,0,400,250"
+        });
+        let staticValues = this.createStatic();
+
+        svg.appendChild(staticValues.x);
+        svg.appendChild(staticValues.y);
+        svg.appendChild(staticValues.title);
 
         let max = Math.max(...this.data);
         for (let i = 1; i < 7; i++) {
@@ -72,7 +79,6 @@ class Barchart {
                 x: plotStart+1,
                 y: 225,
                 "font-size":10,
-                // "text-anchor":"start"
             });
             text.textContent = `${index+1}月`
             //create bars
@@ -88,50 +94,6 @@ class Barchart {
         });
         return svg;
     }
-
-    createAxis(data) {
-        // every 50 - y-axis
-        var y = Math.max(...data);
-        let count = Math.ceil(y / 10);
-
-        // 绘制Y轴
-        let lineY = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        lineY.setAttribute("x1", "30")
-        lineY.setAttribute("x2", "30")
-        lineY.setAttribute("y1", 300)
-        lineY.setAttribute("y2", 350 - 50 - count * 25)
-        lineY.setAttribute("stroke", "#666")
-        chart.appendChild(lineY);
-
-        // 绘制 Y轴 上的数据单位 还有标记线
-        for (let i = 0; i < count + 1; i++) {
-            // 标记线
-            let line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-            line.setAttribute("x1", "30")
-            line.setAttribute("x2", "680")
-            line.setAttribute("y1", 350 - i * 25 - 50)
-            line.setAttribute("y2", 350 - i * 25 - 50)
-            line.setAttribute("stroke", "#b3b3b3")
-            chart.appendChild(line);
-
-            // label
-            let label = document.createElementNS("http://www.w3.org/2000/svg", "text");
-            label.textContent = i * 50;
-            label.setAttribute("x", 25);
-            label.setAttribute("y", 300 - i * 25 + 5)
-            label.setAttribute("text-anchor", "end")
-            chart.appendChild(label);
-        }
-        // xAxis
-        let lineX = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        lineX.setAttribute("x1", "30")
-        lineX.setAttribute("x2", "640")
-        lineX.setAttribute("y1", 300)
-        lineX.setAttribute("y2", 300)
-        lineX.setAttribute("stroke", "#666")
-        chart.appendChild(lineX);
-    }
-
 
     createSVG(name, attributes) {
         let element = document.createElementNS("http://www.w3.org/2000/svg", name)

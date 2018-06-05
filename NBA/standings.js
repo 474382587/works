7b9c0bc02010abfe2e07c730c3b6f7386c5c0226
Joseph Jin <jinyuhui61@hotmail.com>
2018-06-05 16:15:02 -0700

    standings with style

--- NBA/standings.js
+++ NBA/standings.js
@@ -8,7 +8,8 @@ class Standings {
         let standingsList = document.createElement("ol");
         this.standings.forEach(team => {
             let teamStanding = document.createElement('li');
-            teamStanding.innerHTML = `<div class="row"><span>${team.team}</span><span>Tot: ${team.g}</span><span>W: ${team.w}</span><span>L: ${team.l}</span></div>` 
+            teamStanding.classList.add("row")
+            teamStanding.innerHTML = `<span>${team.team}</span><span>Tot: ${team.g}</span><span>W: ${team.w}</span><span>L: ${team.l}</span>` 
             standingsList.appendChild(teamStanding);
         });
         let cofferenceName = document.createElement("h3");

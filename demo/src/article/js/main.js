565b12514b44fe4de9c65ba48d2e35b3342136bb
Joseph Jin <jinyuhui61@hotmail.com>
2018-07-01 13:26:20 -0700

    format

--- demo/src/article/js/main.js
+++ demo/src/article/js/main.js
@@ -1,154 +1,164 @@
-moment.locale('zh-cn');
+moment.locale('zh-cn')
 //計算相對時間
 function calculateTime(millSec) {
-    var date = new Date(millSec * 1000);
-    return moment(date).fromNow();
+  var date = new Date(millSec * 1000)
+  return moment(date).fromNow()
 }
 
 //判断用户是否登录
-window.onload = function () {
-    let cookieArr = document.cookie;
-    if (cookieArr !== "") {
-        document.querySelectorAll(".before-login").forEach(e => {
-            e.classList.add('hide');
-        });
-        document.querySelectorAll(".after-login").forEach(e => {
-            e.classList.remove('hide');
-        });
-    }
-    else {
-        document.querySelectorAll(".after-login").forEach(e => {
-            e.classList.add('hide');
-        });
-        document.querySelectorAll(".before-login").forEach(e => {
-            e.classList.remove('hide');
-        });
-    }
+window.onload = function() {
+  let cookieArr = document.cookie
+  if (cookieArr !== '') {
+    document.querySelectorAll('.before-login').forEach(e => {
+      e.classList.add('hide')
+    })
+    document.querySelectorAll('.after-login').forEach(e => {
+      e.classList.remove('hide')
+    })
+  } else {
+    document.querySelectorAll('.after-login').forEach(e => {
+      e.classList.add('hide')
+    })
+    document.querySelectorAll('.before-login').forEach(e => {
+      e.classList.remove('hide')
+    })
+  }
 }
 
 //更新文章
 function updatePosts(arr) {
-    let posts = document.querySelector('ul.posts');
-    posts.innerHTML = '';
-    for (let i = 0; i < arr.length; i++) {
-        let li = document.createElement('li');
-        // ${arr[i].user_id}
-        li.innerHTML = `<a href='../blog/post.html?topic_id=${arr[i].topic_id}&user_id=${arr[i].user_id}'><div class="left"><p>${arr[i].nick_name} <span>${calculateTime(arr[i].modify_time)}</span></p>${arr[i].title}</div><div class="right"><span><img src="../../static/img/comment-alt.svg" alt="" height="20px">${arr[i].comment_num}</span><span><img src="../../static/img/thumbs-up.svg" alt="" height="20px">${arr[i].like_num}</span></div></a>`
-        posts.appendChild(li);
-    }
+  let posts = document.querySelector('ul.posts')
+  posts.innerHTML = ''
+  for (let i = 0; i < arr.length; i++) {
+    let li = document.createElement('li')
+    // ${arr[i].user_id}
+    li.innerHTML = `<a href='../blog/post.html?topic_id=${
+      arr[i].topic_id
+    }&user_id=${arr[i].user_id}'><div class="left"><p>${
+      arr[i].nick_name
+    } <span>${calculateTime(arr[i].modify_time)}</span></p>${
+      arr[i].title
+    }</div><div class="right"><span><img src="../../static/img/comment-alt.svg" alt="" height="20px">${
+      arr[i].comment_num
+    }</span><span><img src="../../static/img/thumbs-up.svg" alt="" height="20px">${
+      arr[i].like_num
+    }</span></div></a>`
+    posts.appendChild(li)
+  }
 }
-var currentType;
-const tabs = document.querySelectorAll('ul.tab>li');
-const sort = document.querySelectorAll('div.sort>div');
-var previousNode = document.querySelectorAll('ul.tab>li')[0];
-var currentPageNumber = 0;
-const previous = document.querySelector('button.previous');
-const next = document.querySelector('button.next');
-var previousSort = document.querySelectorAll('div.sort>div')[0];
+var currentType
+const tabs = document.querySelectorAll('ul.tab>li')
+const sort = document.querySelectorAll('div.sort>div')
+var previousNode = document.querySelectorAll('ul.tab>li')[0]
+var currentPageNumber = 0
+const previous = document.querySelector('button.previous')
+const next = document.querySelector('button.next')
+var previousSort = document.querySelectorAll('div.sort>div')[0]
 
-
-console.log(previousSort.id);
-requestPosts(0, null, previousSort.id);
+console.log(previousSort.id)
+requestPosts(0, null, previousSort.id)
 //監聽前後頁腳點擊
 previous.addEventListener('click', e => {
-    let posts = document.querySelector('ul.posts');
-    posts.innerHTML = '';
-    requestPosts(currentType, currentPageNumber - 1, previousSort.id);
-});
+  let posts = document.querySelector('ul.posts')
+  posts.innerHTML = ''
+  requestPosts(currentType, currentPageNumber - 1, previousSort.id)
+})
 next.addEventListener('click', e => {
-    let posts = document.querySelector('ul.posts');
-    posts.innerHTML = '';
-    requestPosts(currentType, currentPageNumber + 1, previousSort.id);
-});
+  let posts = document.querySelector('ul.posts')
+  posts.innerHTML = ''
+  requestPosts(currentType, currentPageNumber + 1, previousSort.id)
+})
 //文章分類
 tabs.forEach((el, index) => {
-    el.addEventListener('click', (event) => {
-        //加载中
-        let posts = document.querySelector('ul.posts');
-        posts.innerHTML = '';
-        previousNode.classList.remove('active');
-        el.classList.add('active');
-        previousNode = el;
-        requestPosts(index, null, previousSort.id);
-    });
-});
+  el.addEventListener('click', event => {
+    //加载中
+    let posts = document.querySelector('ul.posts')
+    posts.innerHTML = ''
+    previousNode.classList.remove('active')
+    el.classList.add('active')
+    previousNode = el
+    requestPosts(index, null, previousSort.id)
+  })
+})
 //sort、排序
-sort.forEach(el=>{
-    el.addEventListener('click',event=>{
-        let posts = document.querySelector('ul.posts');
-        posts.innerHTML = '';
-        previousSort.classList.remove('active');
-        el.classList.add('active');
-        previousSort = el;
-        requestPosts(currentType, null, el.id);
-    })
-});
+sort.forEach(el => {
+  el.addEventListener('click', event => {
+    let posts = document.querySelector('ul.posts')
+    posts.innerHTML = ''
+    previousSort.classList.remove('active')
+    el.classList.add('active')
+    previousSort = el
+    requestPosts(currentType, null, el.id)
+  })
+})
 
 //獲取文章
 function requestPosts(type, pageNum, sort) {
-    let loading = document.querySelector('div.loader');
-    loading.classList.remove('hide');
-    let paginationContainer = document.querySelector('div.pagination-container');
+  let loading = document.querySelector('div.loader')
+  loading.classList.remove('hide')
+  let paginationContainer = document.querySelector('div.pagination-container')
 
-    paginationContainer.classList.add('hide');
-    axios({
-        method: 'post',
-      url: 'https://cors-anywhere.herokuapp.com/http://www.ftusix.com/static/data/topicList.php',
-        params: {
-            "type": type,
-            "sort": sort,
-            "page": pageNum + 1,
-            "index": false,
-        }
-    }).then(function (response) {
-        let arr = response.data.data;
-        let listCount = response.data.listCount[0] - '';
+  paginationContainer.classList.add('hide')
+  axios({
+    method: 'post',
+    url:
+      'https://cors-anywhere.herokuapp.com/http://www.ftusix.com/static/data/topicList.php',
+    params: {
+      type: type,
+      sort: sort,
+      page: pageNum + 1,
+      index: false
+    }
+  }).then(function(response) {
+    let arr = response.data.data
+    let listCount = response.data.listCount[0] - ''
 
-        let loading = document.querySelector('div.loader');
-        loading.classList.add('hide');
-        updatePosts(arr);
-        updatePagination(listCount, pageNum);
-        paginationContainer.classList.remove('hide');
-        currentType = type;
-    });
+    let loading = document.querySelector('div.loader')
+    loading.classList.add('hide')
+    updatePosts(arr)
+    updatePagination(listCount, pageNum)
+    paginationContainer.classList.remove('hide')
+    currentType = type
+  })
 }
 //更新頁腳
 function updatePagination(total, index) {
-    index === null ? index = 0 : '';
-    let count = Math.ceil(total / 10);
-    currentPageNumber = index;
+  index === null ? (index = 0) : ''
+  let count = Math.ceil(total / 10)
+  currentPageNumber = index
 
-    if (currentPageNumber === 0) {
-        previous.classList.add('hide');
-        next.classList.remove('hide');
+  if (currentPageNumber === 0) {
+    previous.classList.add('hide')
+    next.classList.remove('hide')
+  } else if (currentPageNumber === count - 1) {
+    next.classList.add('hide')
+    previous.classList.remove('hide')
+  } else {
+    previous.classList.remove('hide')
+    next.classList.remove('hide')
+  }
+  if (count === 1) {
+    previous.classList.add('hide')
+    next.classList.add('hide')
+  }
+  let pagination = document.querySelector('div.pagination')
+  pagination.innerHTML = ''
+  for (let i = 1; i < count + 1; i++) {
+    let span = document.createElement('span')
+    span.innerText = i
+    pagination.appendChild(span)
+  }
 
-    } else if (currentPageNumber === count - 1) {
-        next.classList.add('hide');
-        previous.classList.remove('hide');
-
-    } else {
-        previous.classList.remove('hide');
-        next.classList.remove('hide');
-    }
-    if (count === 1) {
-        previous.classList.add('hide');
-        next.classList.add('hide');
-    }
-    let pagination = document.querySelector('div.pagination');
-    pagination.innerHTML = '';
-    for (let i = 1; i < count + 1; i++) {
-        let span = document.createElement('span');
-        span.innerText = i;
-        pagination.appendChild(span);
-    }
-
-    document.querySelector('div.pagination').querySelectorAll('span')[index].classList.add('active');
-    var numbers = document.querySelectorAll("div.pagination span");
-    numbers.forEach((el, index) => {
-        el.addEventListener('click', e => {
-            let posts = document.querySelector('ul.posts');
-            posts.innerHTML = '';
-            requestPosts(currentType, index, previousSort.id);
-        })
+  document
+    .querySelector('div.pagination')
+    .querySelectorAll('span')
+    [index].classList.add('active')
+  var numbers = document.querySelectorAll('div.pagination span')
+  numbers.forEach((el, index) => {
+    el.addEventListener('click', e => {
+      let posts = document.querySelector('ul.posts')
+      posts.innerHTML = ''
+      requestPosts(currentType, index, previousSort.id)
     })
+  })
 }

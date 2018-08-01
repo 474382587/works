96202f6265d8344c60ff8de6ec0fd9a28f5cc88b
Joseph Jin <jinyuhui61@hotmail.com>
2018-08-01 16:54:17 -0700

    firebase set up

---
+++ frynscoops/firebase.js
@@ -0,0 +1,20 @@
+// // Initialize Firebase
+// var config = {
+//   apiKey: "AIzaSyBwLVZ2bAovT11Q8REEbKwnQ5bsm5SXgAE",
+//   authDomain: "frynscoops.firebaseapp.com",
+//   databaseURL: "https://frynscoops.firebaseio.com",
+//   projectId: "frynscoops",
+//   storageBucket: "",
+//   messagingSenderId: "766135347041"
+// };
+// firebase.initializeApp(config);
+
+// var database = firebase.database()
+
+// function writeUserData (userId, name, email, imageUrl) {
+//   firebase.database().ref('users/' + userId).set({
+//     username: name,
+//     email: email,
+//     profile_picture: imageUrl
+//   });
+// }
